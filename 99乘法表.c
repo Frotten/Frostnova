f34c00acbe6f255038aed09e353d8fc40a2b60ee
Frotten <frottenice@outlook.com>
2024-11-10 00:31:45 +0800

    Add files via upload

---
+++ 99乘法表.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 9)
+//	{
+//		int j ;
+//		for (j = i; j <= 9; j++)
+//		{
+//			printf("%d * %d = %d  ", i, j, i * j);
+//		}
+//		printf("\n");
+//		i++;
+//	}
+//	return 0;
+//}
---
+++ B2029 大象喝水.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//#define Water 0.02
+//int main()
+//{
+//	int r, h;
+//	scanf("%d %d", &h, &r);
+//	double pai = 3.14;
+//	double V = (pai * (r*0.01) * (r * 0.01)) * h * 0.01;
+//	int num = (int)(Water / V);
+//	if (num * V < Water)
+//	{
+//		num++;
+//	}
+//	printf("%d", num);
+//	return 0;
+//}
+////这根本不是cm的桶/这根本不是20升的水
